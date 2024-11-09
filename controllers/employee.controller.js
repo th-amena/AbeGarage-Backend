e5d6a9c50c10c8d0518b87d0f9cec8c63b424a52
Your Name <you@example.com>
2024-11-09 04:16:54 +0300

    other additional code

--- controllers/employee.controller.js
+++ controllers/employee.controller.js
@@ -47,7 +47,7 @@ exports.registerEmployee = async (req, res) => {
     phone: validator.escape(employee_phone),
     email: validator.normalizeEmail(employee_email), // Normalize email
     password: employee_password, // Password will be hashed in the service, so no need to sanitize here
-    active_status: active_employee !== undefined ? active_employee : 1, // Default to active (1) if not provided
+    active_status: active_employee !== undefined ? active_employee : 1,
     role: employee_role || "employee", 
   };
   // Validate: Ensure all required fields are provided

2a25944ab9b71947a97c5321a6b3acfd132fc1fe
KedirABD <kedir_abdurohman@yahoo.com>
2024-11-14 21:11:22 +0300

    updated

--- config/db.config.js
+++ config/db.config.js
@@ -1,20 +1,20 @@
 //Import mysql module
-const mysql = require('mysql2/promise');
+const mysql = require("mysql2/promise");
 //Create a connection to the database
 const connection = mysql.createPool({
-    host:process.env.DB_HOST,
-    user:process.env.DB_USER,
-    password:process.env.DB_PASS,
-    database:process.env.DB_NAME,
-    // socketPath: process.env.DB_SOCKET
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  // socketPath: process.env.DB_SOCKET
 });
 //Check the connection
 connection.getConnection((err) => {
-    if (err) {
-        console.log(err.message);
-    } else {
-        console.log('Database connected');
-    }
-})
+  if (err) {
+    console.log(err.message);
+  } else {
+    console.log("Database connected");
+  }
+});
 //Export the connection
 module.exports = connection;
